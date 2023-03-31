971f004a695e748e98ca243305ad82475689a4f0
Atul Kumar <68207869+atuljaiss@users.noreply.github.com>
2023-03-31 18:37:58 +0530

    Backend Initialized

---
+++ Backend/dsuprotothon/src/main/java/com/dsu/protothon/DsuprotothonApplication.java
@@ -0,0 +1,13 @@
+package com.dsu.protothon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsuprotothonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsuprotothonApplication.class, args);
+	}
+
+}
